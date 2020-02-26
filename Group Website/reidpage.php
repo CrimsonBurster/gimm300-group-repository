cdd8003c14e12f676c18b2547f460bb9bd991859
PebblePriest <43892729+PebblePriest@users.noreply.github.com>
2020-02-25 19:52:28 -0700

    things
    REIDSPAGE

---
+++ Group Website/reidpage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<title>This is Reid's Website</title>
+<head>
+<?php require_once('nav.php');?>
+</head>
+
+<body>
+	
+	<h1> This is My Page </h1>
+	<p>I am Reid Daniels. I am a 21 year old who is currently going to school at BSU in the GIMM program. I am a cook on the side at my families cafe in Greenleaf called Nathan's Greenleaf Cafe. My favorite game of all time is Skyrim, but the game I can not put down is League of Legends. I play all types of games though, since I have a Gameboy, Gameboy Advanced, Nintendo Ds, Ds Lite, 2DS, 3DS, PSP, PS2, PS4, Xbox 360, Xbox One, and a Wii. I know, I play a lot of games. Oh, and of course, a PC, because...</p>
+	<img src=masterrace.jpg title="PC MASTER RACE BITCHES!!!!">
+</body>
+	<?php require_once('footer.php');?>
+
+</html>
