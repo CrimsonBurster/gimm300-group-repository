579ca85558120bdb6a557fed25febc8e521f4269
Ross <44909473+CrimsonBurster@users.noreply.github.com>
2020-02-25 19:57:48 -0700

    updated ross page
    added image and nav/footer

--- Group Website/rossPage.php
+++ Group Website/rossPage.php
@@ -3,8 +3,13 @@
 <head>
 	<title>Ross's Webpage</title>
 </head>
+<?php require_once('nav.php');?>
 <header>Ross's Page</header>
 <body>
-	<p>This is Ross Pataro's placeholder webpage.</p>
+	<p>My name is Ross Pataro. This is a webpage made for gimm300.
+		The picture isn't actually of me, but that can be fixed later :D
+	</p>
+	<img src=dantecover.jpg/>
 </body>
+<?php require_once('footer.php');?>
 </html>
