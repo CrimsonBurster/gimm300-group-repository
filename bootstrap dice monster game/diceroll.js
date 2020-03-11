3fb9513689d6f44121e563d38cce01c13e684e3a
Ross <44909473+CrimsonBurster@users.noreply.github.com>
2020-03-10 19:20:58 -0600

    added code to make attacks work

--- bootstrap dice monster game/diceroll.js
+++ bootstrap dice monster game/diceroll.js
@@ -1,4 +1,12 @@
+var playerHealth = 20;
+var monsterHealth = 30;
+var bothRolls;
+var attackDamage;
+var monsterDamage;
+var damage;
+
 var rollTheDice = function(){
+//rolls the dice
   var pictureTarget = document.getElementById("picture");
   var pictureTarget2 = document.getElementById("picture2");
   //pictureTarget.src = "";
@@ -8,8 +16,35 @@ var rollTheDice = function(){
 pictureTarget.src = "dice"+ rollDice + ".png";
 pictureTarget2.src = "dice"+ rollDice2 + ".png";
 
-//window.alert("number is changing");
+//hold both dice
+bothRolls = rollDice.value + rollDice2.value;
 
+//check values to determine the attack
+playerAttack();
+monsterAttack();
+healthChange();
+
+}
+
+function playerAttack(){
+  if(bothRolls <= 5){
+    attackDamage = 2;
+  }
+  else if(bothRolls >= 6 && bothRolls <= 10){
+    attackDamage = 4;
+  }
+  else{
+    attackDamage = 7;
+  }
+}
+
+function monsterAttack(){
+  monsterDamage = Math.floor(Math.random() * 7) + 2;
+}
+
+function healthChange(){
+  playerHealth -= monsterDamage;
+  monsterHealth -= attackDamage;
 }
 
 document.getElementById("button1").onclick = rollTheDice
